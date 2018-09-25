0b3dc328aea52e0c46e258c95e59c13137e9c2e8
Atul Sharma <atulkumarsharma1986@gmail.com>
2018-09-25 12:36:13 +0530

    code changes for the soap client request

---
+++ spring-soap-client/src/main/java/org/spring/soap/client/article/ArticleClient.java
@@ -0,0 +1,7 @@
+package org.spring.soap.client.article;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+public class ArticleClient extends WebServiceGatewaySupport {
+
+}
